8f3312271e3067b61635cfc23786cd95d47d2fa5
Grzegorz Strzelczyk <gstrzelczyk@gmail.com>
2024-10-24 00:18:53 +0200

    test połączenia z bazą, Instalacja bootstrapa oraz odrębny szablon header i footer

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_NAME', 'zadanie_rekrutacyjne_it_wid');
+define('DB_PASS', '');
+?>
--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
+@require_once 'config.php';
+@include_once 'szablon/header.php';
 
+// Połączenie z bazą danych
+$db = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Wyświetlanie pierwszych 3 wierszy
+$q = $db->prepare('SELECT * FROM klienci LIMIT 3');
+$q->execute();
+$r = $q->get_result();
+echo "<pre>Pierwsze 3 wierszy:\n";
+while ($row = $r->fetch_assoc()) {
+    print_r($row);
+}
+echo "</pre>";
+
+// Sprawdzanie, czy jest więcej niż 3 wierszy
+$q = $db->prepare('SELECT COUNT(*) as total FROM klienci');
+$q->execute();
+$r = $q->get_result();
+$row = $r->fetch_assoc();
+$totalRows = $row['total'];
+
+if ($totalRows > 3) {
+    // Wyświetlanie kolejnych 3 wierszy
+    $q = $db->prepare('SELECT * FROM klienci LIMIT 3 OFFSET 3');
+    $q->execute();
+    $r = $q->get_result();
+    echo "<pre>Kolejne 3 wierszy:\n";
+    while ($row = $r->fetch_assoc()) {
+        print_r($row);
+    }
+    echo "</pre>";
+}
+@include_once 'szablon/footer.php';
 ?>
