1e4c83011752a781716d0abc33a4f46146116406
Grzegorz Strzelczyk <gstrzelczyk@gmail.com>
2024-10-28 21:10:22 +0100

    Dodano formularz dodawania nowego pakietu w widoku szczegółów klienta. Nie dodano ejsczez obsługi zapytań z tego formularza

--- szablon/details.php
+++ szablon/details.php
@@ -153,6 +153,9 @@
                         </tbody>
                     </table>
                 </div>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addpackage">
+                    Dodaj pakiet
+                </button>
             </div>
         </div>
     </div>
@@ -345,6 +348,253 @@
     </div>
 </div>
 
+<div class="modal" id="addpackage" tabindex="-1" aria-labelledby="addpackageLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Dodaj pakiet dla klienta "<?php echo $klient['klient']['nazwa']; ?>"</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="/?action=addpackage&idc=<?php echo $klient['klient']['id']; ?>" method="post" onsubmit="return validateForm()">
+                    <label class="form-label" for="pakiet_checkbox">Dodaj nowy pakiet:</label>
+                    <input type="checkbox" class="form-check-input" id="pakiet_checkbox" name="pakiet_checkbox" onclick="togglePakietFields()"><br>
+                    
+                    <div id="pakiet_select">
+                        <label class="form-label" for="pakiet">Wybierz pakiet:</label>
+                        <select class="form-select" id="pakiet" name="pakiet">
+                            <!-- Opcje zostaną załadowane dynamicznie -->
+                        </select><br>
+                    </div>
+                    
+                    <div id="pakiet_fields" style="display:none;">
+                        <label class="form-label" for="nowy_pakiet">Dodaj nowy pakiet:</label><br>
+                        <label class="form-label" for="nazwa_pakiet">Nazwa:</label>
+                        <input class="form-control" type="text" id="nazwa_pakiet" name="nazwa_pakiet" data-req="req"><br>
+                        <label class="form-label" for="cena_pakiet">Cena:</label>
+                        <input class="form-control" type="number" step="0.01" id="cena_pakiet" name="cena_pakiet"><br>
+                        <label class="form-label" for="czas_trwania_pakiet">Czas trwania (w miesiącach):</label>
+                        <input class="form-control" type="number" id="czas_trwania_pakiet" name="czas_trwania_pakiet" onchange="updatePakietDates()"><br>
+                        <label class="form-label" for="data_sprzedazy_pakiet">Data sprzedaży:</label>
+                        <input class="form-control" type="date" id="data_sprzedazy_pakiet" name="data_sprzedazy_pakiet" value="<?php echo date('Y-m-d'); ?>"><br>
+                        <label class="form-label" for="data_wygasniecia_pakiet">Data wygaśnięcia:</label>
+                        <input class="form-control" type="date" id="data_wygasniecia_pakiet" name="data_wygasniecia_pakiet" readonly><br>
+                    </div>
+                
+                    <legend>Sprzedawca pakietu</legend>
+                    <label class="form-label" for="sprzedawca_checkbox">Dodaj nowego sprzedawcę:</label>
+                    <input type="checkbox" class="form-check-input" id="sprzedawca_checkbox" name="sprzedawca_checkbox" onclick="toggleSprzedawcaFields()"><br>
+                    
+                    <div id="sprzedawca_select">
+                        <label class="form-label" for="sprzedawca">Wybierz sprzedawcę:</label>
+                        <select class="form-select" id="sprzedawca" name="sprzedawca">
+                            <!-- Opcje zostaną załadowane dynamicznie -->
+                        </select><br>
+                    </div>
+                    
+                    <div id="sprzedawca_fields" style="display:none;">
+                        <label class="form-label" for="nowy_sprzedawca">Dodaj nowego sprzedawcę:</label><br>
+                        <label class="form-label" for="imie_sprzedawca">Imię:</label>
+                        <input class="form-control" type="text" id="imie_sprzedawca" name="imie_sprzedawca" data-req="req"><br>
+                        <label class="form-label" for="nazwisko_sprzedawca">Nazwisko:</label>
+                        <input class="form-control" type="text" id="nazwisko_sprzedawca" name="nazwisko_sprzedawca" data-req="req"><br>
+                        <label class="form-label" for="mail_sprzedawca">Email:</label>
+                        <input class="form-control" type="email" id="mail_sprzedawca" name="mail_sprzedawca"><br>
+                        <label class="form-label" for="telefon_sprzedawca">Telefon:</label>
+                        <input class="form-control" type="tel" id="telefon_sprzedawca" name="telefon_sprzedawca"><br>
+                        <label class="form-label" for="adres_sprzedawca">Adres:</label>
+                        <textarea class="form-control" id="adres_sprzedawca" name="adres_sprzedawca"></textarea><br>
+                        <label class="form-label" for="stanowisko_sprzedawca">Stanowisko:</label>
+                        <legend>Stanowisko</legend>
+                        <label class="form-label" for="sprzedawca_stanowisko_checkbox">Dodaj nowe stanowisko:</label>
+                        <input type="checkbox" class="form-check-input" id="sprzedawca_stanowisko_checkbox" name="sprzedawca_stanowisko_checkbox" onclick="toggleStanowiskoFields()"><br>
+                        <div id="sprzedawca_stanowisko_select">
+                            <label class="form-label" for="stanowisko_sprzedawca">Wybierz stanowisko:</label>
+                            <select class="form-select" id="stanowisko_sprzedawca" name="stanowisko_sprzedawca">
+                                <!-- Opcje zostaną załadowane dynamicznie -->
+                            </select><br>
+                        </div>
+                        <div id="sprzedawca_stanowisko_fields" style="display:none;">
+                            <label class="form-label" for="sprzedawca_nowe_stanowisko">Dodaj nowe stanowisko:</label><br>
+                            <input class="form-control" type="text" id="sprzedawca_nowe_stanowisko" name="sprzedawca_nowe_stanowisko" data-req="req"><br>
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Dodaj pakiet</button>
+                </form>
+
+                <script>
+                function togglePakietFields() {
+                    var checkbox = document.getElementById('pakiet_checkbox');
+                    var pakietSelect = document.getElementById('pakiet_select');
+                    var pakietFields = document.getElementById('pakiet_fields');
+                    var requiredFields = pakietFields.querySelectorAll('[data-req="req"]');
+                    
+                    if (checkbox.checked) {
+                        pakietSelect.style.display = 'none';
+                        pakietFields.style.display = 'block';
+                        requiredFields.forEach(function(field) {
+                            field.setAttribute('required', 'required');
+                        });
+                    } else {
+                        pakietSelect.style.display = 'block';
+                        pakietFields.style.display = 'none';
+                        requiredFields.forEach(function(field) {
+                            field.removeAttribute('required');
+                        });
+                        loadPakiety();
+                    }
+                }
+
+                function toggleSprzedawcaFields() {
+                    var checkbox = document.getElementById('sprzedawca_checkbox');
+                    var sprzedawcaSelect = document.getElementById('sprzedawca_select');
+                    var sprzedawcaFields = document.getElementById('sprzedawca_fields');
+                    var requiredFields = sprzedawcaFields.querySelectorAll('[data-req="req"]:not(#sprzedawca_nowe_stanowisko)');
+                    
+                    if (checkbox.checked) {
+                        sprzedawcaSelect.style.display = 'none';
+                        sprzedawcaFields.style.display = 'block';
+                        requiredFields.forEach(function(field) {
+                            field.setAttribute('required', 'required');
+                        });
+                    } else {
+                        sprzedawcaSelect.style.display = 'block';
+                        sprzedawcaFields.style.display = 'none';
+                        requiredFields = sprzedawcaFields.querySelectorAll('[data-req="req"]');
+                        requiredFields.forEach(function(field) {
+                            field.removeAttribute('required');
+                        });
+                        loadWorkers();
+                    }
+                }
+
+                function toggleStanowiskoFields() {
+                    var checkbox = document.getElementById('opiekun_stanowisko_checkbox');
+                    var checkboxSprzedawca = document.getElementById('sprzedawca_stanowisko_checkbox');
+                    var stanowiskoSelect = document.getElementById('stanowisko_select');
+                    var stanowiskoSprzedawcaSelect = document.getElementById('sprzedawca_stanowisko_select');
+                    var stanowiskoFields = document.getElementById('opiekun_stanowisko_fields');
+                    var stanowiskoSprzedawcaFields = document.getElementById('sprzedawca_stanowisko_fields');
+                    var requiredFields = stanowiskoFields.querySelectorAll('[data-req="req"]');
+                    var requiredFieldsSprzedawca = stanowiskoSprzedawcaFields.querySelectorAll('[data-req="req"]');
+                    
+                    if (checkbox.checked) {
+                        stanowiskoSelect.style.display = 'none';
+                        stanowiskoFields.style.display = 'block';
+                        requiredFields.forEach(function(field) {
+                            field.setAttribute('required', 'required');
+                        });
+                    } else {
+                        stanowiskoSelect.style.display = 'block';
+                        stanowiskoFields.style.display = 'none';
+                        requiredFields.forEach(function(field) {
+                            field.removeAttribute('required');
+                        });
+                        loadStanowiska();
+                    }
+                    if (checkboxSprzedawca.checked) {
+                        stanowiskoSprzedawcaSelect.style.display = 'none';
+                        stanowiskoSprzedawcaFields.style.display = 'block';
+                        requiredFieldsSprzedawca.forEach(function(field) {
+                            field.setAttribute('required', 'required');
+                        });
+                    } else {
+                        stanowiskoSprzedawcaSelect.style.display = 'block';
+                        stanowiskoSprzedawcaFields.style.display = 'none';
+                        requiredFieldsSprzedawca.forEach(function(field) {
+                            field.removeAttribute('required');
+                        });
+                        loadStanowiska();
+                    }
+                }
+
+                function updatePakietDates() {
+                    var dataSprzedazy = document.getElementById('data_sprzedazy_pakiet').value;
+                    var czasTrwania = document.getElementById('czas_trwania_pakiet').value;
+                    if (dataSprzedazy && czasTrwania) {
+                        var dataSprzedazyDate = new Date(dataSprzedazy);
+                        dataSprzedazyDate.setMonth(dataSprzedazyDate.getMonth() + parseInt(czasTrwania));
+                        var dataWygasniecia = dataSprzedazyDate.toISOString().split('T')[0];
+                        document.getElementById('data_wygasniecia_pakiet').value = dataWygasniecia;
+                    }
+                }
+
+                document.getElementById('data_sprzedazy_pakiet').addEventListener('change', updatePakietDates);
+
+                function loadPakiety() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', '/index.php?action=getpakiety', true);
+                    xhr.onload = function() {
+                        if (xhr.status === 200) {
+                            var pakiety = JSON.parse(xhr.responseText);
+                            var select = document.getElementById('pakiet');
+                            select.innerHTML = ''; // Clear existing options
+                            pakiety.forEach(function(pakiet) {
+                                var option = document.createElement('option');
+                                option.value = pakiet.id;
+                                option.textContent = pakiet.nazwa + ' (' + pakiet.cena + ' PLN)';
+                                select.appendChild(option);
+                            });
+                        } else {
+                            console.error('Failed to load pakiety');
+                        }
+                    };
+                    xhr.send();
+                }
+
+                function loadStanowiska() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', '/index.php?action=getstanowiska', true);
+                    xhr.onload = function() {
+                        if (xhr.status === 200) {
+                            var stanowiska = JSON.parse(xhr.responseText);
+                            var select = document.getElementById('stanowisko_opiekun');
+                            var sprzedawcaSelect = document.getElementById('stanowisko_sprzedawca');
+                            select.innerHTML = ''; // Clear existing options
+                            sprzedawcaSelect.innerHTML = ''; // Clear existing options
+                            stanowiska.forEach(function(stanowisko) {
+                                var option = document.createElement('option');
+                                option.value = stanowisko.id;
+                                option.textContent = stanowisko.nazwa;
+                                select.appendChild(option);
+                                sprzedawcaSelect.appendChild(option.cloneNode(true)); // Clone the option for sprzedawca
+                            });
+                        } else {
+                            console.error('Failed to load stanowiska');
+                        }
+                    }
+                    xhr.send();
+                }
+
+                function loadWorkers() {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', '/index.php?action=getworkers', true);
+                    xhr.onload = function() {
+                        if (xhr.status === 200) {
+                            var workers = JSON.parse(xhr.responseText);
+                            var opiekunSelect = document.getElementById('opiekun');
+                            var sprzedawcaSelect = document.getElementById('sprzedawca');
+                            opiekunSelect.innerHTML = ''; // Clear existing options
+                            sprzedawcaSelect.innerHTML = ''; // Clear existing options
+                            workers.forEach(function(worker) {
+                                var option = document.createElement('option');
+                                option.value = worker.id;
+                                option.textContent = worker.imie + ' ' + worker.nazwisko + ' (' + worker.stanowisko + ')';
+                                opiekunSelect.appendChild(option);
+                                sprzedawcaSelect.appendChild(option.cloneNode(true)); // Clone the option for sprzedawca
+                            });
+                        } else {
+                            console.error('Failed to load workers');
+                        }
+                    };
+                    xhr.send();
+                }
+                </script>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
 function toggleKontaktFields() {
     var checkbox = document.getElementById('kontakt_checkbox');
@@ -403,6 +653,7 @@ function validateForm() {
 document.addEventListener('DOMContentLoaded', function() {
                     loadWorkers();
                     loadStanowiska();
+                    loadPakiety();
                 });
 </script>
 <?php
